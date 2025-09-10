948693b915b8e5ccfaaf9648312eb3531eb74903
[dyad] <git@dyad.sh>
2025-09-10 18:25:35 +0530

    Reverted all changes back to version f1e5179ba23dbded5b63d0ec4642031bf298c436

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RajuTextileLandingPage from "./pages/RajuTextileLandingPage"; // Import the new landing page
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<RajuTextileLandingPage />} /> {/* Set RajuTextileLandingPage as the default route */}
+          <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
